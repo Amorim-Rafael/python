f01cbfddad85f0491055dad3fb15c384f5a8f40f
Amorim-Rafael <rafaelbrsa@gmail.com>
2017-05-15 13:23:36 -0300

    aula 21

--- rodaprograma.py
+++ rodaprograma.py
@@ -27,16 +27,6 @@ while pode_parar == False:
 # Mostrar a menor idade calculada
 # Mostrar a maior idade calculada
 # Mostrar a media de idade calculada
-# Mostrar a quantidade de nascimentos por decada
-
-
-# lista_idades = []
-# Preenchimento comum de uma lista 
-# for item in lista_entrevistados:
-#     lista_idades.append(item.idade)
-
-# menor_idade = min(lista_idades)
-# maior_idade = max(lista_idades)
 
 # Lista por Compreensão
 # lista = [<expresao para valor> <loop> <expressao para o loop>]
@@ -45,6 +35,19 @@ menor_idade = min([item.idade for item in lista_entrevistados])
 maior_idade = max([item.idade for item in lista_entrevistados])
 media_adulto = statistics.median_high([item.idade for item in lista_entrevistados if item.idade >= 18])
 
-print('Menor idade é:',menor_idade)
-print('Maior idade é:',maior_idade)
-print('Media idade adulto:',media_adulto)
+# Mostrar a quantidade de nascimentos por decada
+# O que temos: [1971, 1982, 2003, 1995, 1976]
+
+# 1º passo: converter anos em decadas
+# 2º passo: criar uma lista nova com decadas sem repetir
+# 3º passo: contar as decadas da lista original na lista nova
+
+# O que queremos: [1970:2, 1980:5, 1990:1, 2000:2]
+
+# 1985 / 10 = 198,5 int > 198 * 10 = 1980
+lista_decadas = [int(item.ano_informado/10)*10 for item in lista_entrevistados]
+# set(): lista que valores não se repetem e não podem ser ordenadas
+print('lista_decadas:',lista_decadas)
+# print('Menor idade é:',menor_idade)
+# print('Maior idade é:',maior_idade)
+# print('Media idade adulto:',media_adulto)
